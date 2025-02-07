4345c7f2ab2767cecce40fc6e5413cf159b5dfa3
Nimesh Mori <morinimesh007@gmail.com>
2025-02-08 01:15:32 +0530

    Files Upload

---
+++ web.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+
+def model_prediction(test_image):
+    model = tf.keras.models.load_model("trained_plant_disease_model.keras")
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size = (128, 128))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr]) #convert single image to batch
+    predictions = model.predict(input_arr)
+    return np.argmax(predictions) #return index of max element
+
+st.sidebar.title("Potato Leaf Disease Detection")
+app_mode = st.sidebar.selectbox("select page", ["Home","Disease Recognition"])
+
+from PIL import Image
+img = Image.open("Disease.jpg")
+st.image(img)
+
+if(app_mode == 'HOME'):
+    st.markdown("<h1 style = 'text-align: center;'>Plant Disease Detection System for Sustainable Agriculture", unsafe_allow_html = True)
+elif(app_mode == 'Disease Recognition'):
+    st.header('Potato Leaf Detection System') 
+
+test_image = st.file_uploader ('choose an image:')
+if(st.button('Show Image')):
+    st.image(test_image, width = 4, use_container_width = True)
+
+if (st.button('Predict')):
+    st.snow()
+    st.write('our prediction')
+    result_index = model_prediction(test_image)
+    class_name = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+    st.success('Model is predicting its a {}'.format(class_name[result_index]))
